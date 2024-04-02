d8847649da7992ad4071ad8ea518d2c4ee44dc24
naryeong-ko <naryung26@gmail.com>
2024-04-02 11:38:43 +0900

    [🌱study] 회원 웹 기능 - 등록

--- 김영한의 스프링 강의/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ 김영한의 스프링 강의/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,29 +1,34 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
 
     private final MemberService memberService;
 
-    /*
-    // 필드 주입(단점 : 방식 수정 불가)
-    @Autowired private final MemberService memberService;
-
-    // 세터 주입(단점 : 누군가가 수정할 수 있음)
-    @Autowired
-    public void setMemberService(MemberService memberService) {
-        this.memberService = memberService;
-    }
-    */
-
-    // 생성자 주입(의존관계가 실행 중에 동적으로 변하는 경우는 거의 없으므로 이 방식 권장)
     @Autowired // 스프링 컨테이너에서 꺼내 넣기 때문에 불필요한 추가 생성 방지
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 }
