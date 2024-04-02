d682cd92782c98b5fd3e0c25161b1ea4604d126e
naryeong-ko <naryung26@gmail.com>
2024-04-02 10:38:38 +0900

    [🌱study] 자바 코드로 직접 스프링 빈 등록하기
    - 필드 주입
    - 세터 주입
    - 생성자 주입

--- 김영한의 스프링 강의/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ 김영한의 스프링 강의/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -9,10 +9,21 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    /*
+    // 필드 주입(단점 : 방식 수정 불가)
+    @Autowired private final MemberService memberService;
+
+    // 세터 주입(단점 : 누군가가 수정할 수 있음)
+    @Autowired
+    public void setMemberService(MemberService memberService) {
+        this.memberService = memberService;
+    }
+    */
+
+    // 생성자 주입(의존관계가 실행 중에 동적으로 변하는 경우는 거의 없으므로 이 방식 권장)
     @Autowired // 스프링 컨테이너에서 꺼내 넣기 때문에 불필요한 추가 생성 방지
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
-
 
 }
