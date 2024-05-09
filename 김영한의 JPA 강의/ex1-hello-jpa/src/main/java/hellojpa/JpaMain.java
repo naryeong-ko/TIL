d4adf89e12870196c607b0ade09f684570009e1b
naryeong-ko <naryung26@gmail.com>
2024-05-09 14:41:14 +0900

    [🛢️init] Hello JPA - 애플리케이션 개발

--- 김영한의 JPA 강의/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 김영한의 JPA 강의/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,33 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+//            Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+
+            for(Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
