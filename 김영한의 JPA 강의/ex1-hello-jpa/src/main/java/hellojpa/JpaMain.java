aa40051d394de1debc2a61c737b77f9644b494d8
naryeong-ko <naryung26@gmail.com>
2024-05-16 09:29:06 +0900

    [️🛢️study] 단방향 연관관계

--- 김영한의 JPA 강의/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 김영한의 JPA 강의/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -13,6 +13,19 @@ public class JpaMain {
         tx.begin();
 
         try {
+            // 저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
+            em.persist(member);
+
+            Member findMember = em.find(Member.class, member.getId());
+
+            Team findTeam = findMember.getTeam();
 
             tx.commit();
         } catch (Exception e) {
--- 김영한의 JPA 강의/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 김영한의 JPA 강의/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,33 +2,41 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
-import java.util.Date;
-
 @Entity
 public class Member {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "USERNAME", nullable = false)
     private String username;
 
-    private int age;
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob
-    private String description;
-
-    public Member() {
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }
