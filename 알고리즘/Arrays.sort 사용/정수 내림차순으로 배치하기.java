91078a7c309dbec554d3e0b544434c40d3fd24ec
naryeong-ko <99406992+naryeong-ko@users.noreply.github.com>
2024-05-26 21:27:09 +0900

    [Algo] Arrays.sort 사용 문제 풀이
    [Algo] 프로그래머스 정수 내림차순으로 배치하기 문제 풀이

---
+++ 알고리즘/Arrays.sort 사용/정수 내림차순으로 배치하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public long solution(long n) {
+        char[] chars = Long.toString(n).toCharArray();
+        Arrays.sort(chars);
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = chars.length - 1; i >= 0; i--) {
+            sb.append(chars[i]);
+        }
+        
+        return Long.parseLong(sb.toString());
+    }
+}
