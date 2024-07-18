ec91834f0802be55ad5371a172ecc0374467b494
naryeong-ko <naryung26@gmail.com>
2024-07-18 11:34:28 +0900

    [🛢️study] 실전 예제 4 - 상속관계 매핑

--- 김영한의 JPA 강의/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ 김영한의 JPA 강의/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "ORDERS")
-public class Order {
+public class Order extends BaseEntity {
 
     @Id @GeneratedValue
     @Column(name = "ORDER_ID")
@@ -34,35 +34,5 @@ public class Order {
         orderItem.setOrder(this);
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Member getMember() {
-        return member;
-    }
-
-    public void setMember(Member member) {
-        this.member = member;
-    }
-
-    public LocalDateTime getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public OrderStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(OrderStatus status) {
-        this.status = status;
-    }
+    // 게터 세터 넣기
 }
