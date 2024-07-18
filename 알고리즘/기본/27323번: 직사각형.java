72835066a716544e03b2cd545cb4ce9df024e0d9
naryeong-ko <naryung26@gmail.com>
2024-07-18 09:48:42 +0900

    [Algo] 기본 문제 풀이
    [Algo] 백준 27323번: 직사각형 문제 풀이

---
+++ 알고리즘/기본/27323번: 직사각형.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()));
+        br.close();
+    }
+
+}
