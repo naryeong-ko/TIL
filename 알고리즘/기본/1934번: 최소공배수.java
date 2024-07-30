e584e8c95d22105f35919d4bc31b176ee0e5a189
naryeong-ko <naryung26@gmail.com>
2024-07-30 11:16:14 +0900

    [Algo] 기본 문제 풀이
    [Algo] 백준 1934번: 최소공배수 문제 풀이

---
+++ 알고리즘/기본/1934번: 최소공배수.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        while(T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            bw.write(A * B / gcd(A, B) + "\n");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    public static int gcd(int a, int b) {
+        if(b == 0) {
+            return a;
+        }
+
+        return gcd(b, a % b);
+    }
+}
