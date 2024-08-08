88bb8ed4d7031a6a679d6d394eefc64654e9a9f8
naryeong-ko <naryung26@gmail.com>
2024-08-08 11:05:06 +0900

    [Algo] 기본 문제 풀이
    [Algo] 백준 19532번: 수학은 비대면강의입니다 문제 풀이

---
+++ 알고리즘/기본/19532번: 수학은 비대면강의입니다.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        bw.write((c * e - b * f) / (a * e - b * d) + " ");
+        bw.write((c * d - a * f) / (b * d - a * e) + "");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
