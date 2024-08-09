5be8abd719aa62d6d12f2d9e16f73fd19e6e3ee1
naryeong-ko <naryung26@gmail.com>
2024-08-09 13:44:43 +0900

    [Algo] 기본 문제 풀이
    [Algo] 백준 2750번: 수 정렬하기 문제 풀이

---
+++ 알고리즘/기본/2750번: 수 정렬하기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(nums);
+
+        for(int i = 0; i < N; i++) {
+            bw.write(nums[i] + "\n");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
