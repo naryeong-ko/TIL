e4b422933841fa8f94b9ce70146f847ffe6761a9
naryeong-ko <naryung26@gmail.com>
2024-08-18 15:56:24 +0900

    [️🛢️study] 프록시

--- 김영한의 JPA 강의/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 김영한의 JPA 강의/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import jakarta.persistence.*;
+import org.hibernate.Hibernate;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,24 +17,31 @@ public class JpaMain {
         tx.begin();
 
         try {
-
-            Member member = new Member();
-            member.setUsername("user1");
-            member.setCreatedBy("kim");
-            member.setCreatedDate(LocalDateTime.now());
-
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("member1");
+            em.persist(member1);
 
             em.flush();
             em.clear();
 
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember = " + refMember.getClass());
+            Hibernate.initialize(refMember); // 강제 초기화
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
+            System.out.println("e = " + e);
         } finally {
             em.close();
         }
 
         emf.close();
     }
+
+    private static void logic(Member m1, Member m2) {
+        System.out.println("m1 == m2: " + (m1 instanceof Member));
+        System.out.println("m1 == m2: " + (m2 instanceof Member));
+    }
+
 }
