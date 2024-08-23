bc3ca52626f7ec089a3b4fa140d6c71dbd5a0e10
naryeong-ko <naryung26@gmail.com>
2024-08-23 09:49:06 +0900

    [Algo] 기본 문제 풀이
    [Algo] 백준 24263번: 알고리즘 수업 - 알고리즘의 수행 시간 2 문제 풀이

---
+++ 알고리즘/기본/24263번: 알고리즘 수업 - 알고리즘의 수행 시간 2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print(br.readLine() + "\n1");
+
+        br.close();
+    }
+
+}
