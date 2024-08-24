cb8d3b6ea622e8eddbedc68d19ce5e6dcc230aea
naryeong-ko <naryung26@gmail.com>
2024-08-24 11:55:54 +0900

    [️🛢️study] 값 타입의 비교
    - 동일성(identity) 비교: 인스턴스의 참조 값을 비교, == 사용
    - 동등성(equivalence) 비교: 인스턴스의 값을 비교, equals 사용
    - 값 타입은 a.equals(b)를 사용해서 동등성 비교를 해야 함
    - 값 타입의 equals() 메소드를 적절하게 재정의(주로 모든 필드 사용)

--- 김영한의 JPA 강의/ex1-hello-jpa/src/main/java/hellojpa/Address.java
+++ 김영한의 JPA 강의/ex1-hello-jpa/src/main/java/hellojpa/Address.java
@@ -3,6 +3,8 @@ package hellojpa;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
+import java.util.Objects;
+
 @Embeddable
 public class Address {
     private String city;
@@ -42,5 +44,18 @@ public class Address {
 
     private void setZipcode(String zipcode) {
         this.zipcode = zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zipcode, address.zipcode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zipcode);
     }
 }
