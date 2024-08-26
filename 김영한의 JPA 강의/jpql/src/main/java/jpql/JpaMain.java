ceb921f51959c52d29f7977a42dbe2e794e9b8bf
naryeong-ko <naryung26@gmail.com>
2024-08-26 18:18:20 +0900

    [🛢️init] JPQL 기본 문법과 쿼리 API
    JPQL 문법
    - 엔티티와 속성은 대소문자 구분 O
    - JPQL 키워드는 대소문자 구분 X
    - 엔티티 이름 사용(테이블 이름 아님)
    - 별칭 필수(as는 생략 가능)
    
    TypeQuery, Query
    - TypeQuery: 반환 타입이 명확할 때 사용
    - Query: 반환 타입이 명확하지 않을 때 사용
    
    query.getResultList(), query.getSingleResult()
    - query.getResultList(): 결과가 하나 이상일 때 리스트, 결과가 없으면 빈 리스트 반환
    - query.getSingleResult():결과가 정확히 하나, 단일 객체 반환. 결과가 없거나 둘 이상이면 Exception
    
    파라미터 바인딩
    - 이름 기준
    - 위치 기준(웬만하면 쓰지 X)

--- 김영한의 JPA 강의/jpql/src/main/java/jpql/JpaMain.java
+++ 김영한의 JPA 강의/jpql/src/main/java/jpql/JpaMain.java
@@ -2,6 +2,8 @@ package jpql;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -15,7 +17,49 @@ public class JpaMain {
         try {
             Member member = new Member();
             member.setUsername("member1");
+            member.setAge(10);
             em.persist(member);
+
+            /*
+            <<TypeQuery, Query>>
+                - TypeQuery: 반환 타입이 명확할 때 사용
+                - Query: 반환 타입이 명확하지 않을 때 사용
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+            Query query3 = em.createQuery("select m.username, m.age from Member m");
+            */
+
+            /*
+            <<query.getResultList()>>
+                - 결과가 하나 이상일 때, 리스트 반환
+                - 결과가 없으면 빈 리스트 반환
+            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
+            List<Member> resultList = query.getResultList();
+
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
+            */
+
+            /*
+            <<query.getSingleResult()>>
+                - 결과가 정확히 하나, 단일 객체 반환
+                - 결과가 없으면 NoResultException
+                - 둘 이상이면 NonUniqueResultException
+            TypedQuery<Member> query = em.createQuery("select m from Member m where m.id = 10", Member.class);
+            Member result = query.getSingleResult();
+
+            System.out.println("member1 = " + result);
+            */
+
+            /*
+            <<파라미터 바인딩>>
+             - 이름 기반
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                    .setParameter("username", "member1")
+                    .getSingleResult();
+            System.out.println("result = " + result.getUsername());
+            */
 
             tx.commit();
         } catch (Exception e) {
