f2b0ea2fa5ab267a2e8929d369c7113201477eed
naryeong-ko <naryung26@gmail.com>
2024-08-31 12:21:53 +0900

    [Algo] 기본 문제 풀이
    [Algo] 프로그래머스 평행 문제 풀이

---
+++ 알고리즘/기본/평행.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[][] dots) {
+        if(checkDots(dots, 0, 1, 2, 3) || checkDots(dots, 0, 2, 1, 3) || checkDots(dots, 0, 3, 1, 2)) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    public boolean checkDots(int[][] dots, int a, int b, int c, int d) {
+        return (dots[a][0] - dots[b][0]) * (dots[c][1] - dots[d][1]) == (dots[a][1] - dots[b][1]) * (dots[c][0] - dots[d][0]);
+    }
+}
