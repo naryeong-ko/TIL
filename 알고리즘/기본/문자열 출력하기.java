4450aef1114dcd68e4f9189ccc833065bc28f52a
naryeong-ko <naryung26@gmail.com>
2024-09-05 10:25:59 +0900

    [Algo] 기본 문제 풀이
    [Algo] 프로그래머스 문자열 출력하기 문제 풀이

---
+++ 알고리즘/기본/문자열 출력하기.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(br.readLine());
+        br.close();
+    }
+
+}
