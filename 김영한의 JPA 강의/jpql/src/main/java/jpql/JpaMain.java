7a325d4e7ea34ed988f382098bf901fad4e51aeb
naryeong-ko <naryung26@gmail.com>
2024-09-06 14:27:26 +0900

    [🛢️init] 프로젝션 - 여러 값 조회
    - new 명령어로 여러 값 조회 -> 단순 값을 DTO로 바로 조회(패키지 명을 포함한 전체 클래스 명 입력, 순서와 타입이 일치하는 생성자 필요)

--- 김영한의 JPA 강의/jpql/src/main/java/jpql/JpaMain.java
+++ 김영한의 JPA 강의/jpql/src/main/java/jpql/JpaMain.java
@@ -20,46 +20,21 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            /*
-            <<TypeQuery, Query>>
-                - TypeQuery: 반환 타입이 명확할 때 사용
-                - Query: 반환 타입이 명확하지 않을 때 사용
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-            Query query3 = em.createQuery("select m.username, m.age from Member m");
-            */
+            em.flush();
+            em.clear();
 
-            /*
-            <<query.getResultList()>>
-                - 결과가 하나 이상일 때, 리스트 반환
-                - 결과가 없으면 빈 리스트 반환
-            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-            List<Member> resultList = query.getResultList();
+            /* 스칼라 타입 프로젝션
+            em.createQuery("select m.username, m.age from Member m")
+                    .getResultList();
+             */
 
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
-            }
-            */
+            // new 명령어로 여러 값 조회 -> 단순 값을 DTO로 바로 조회(순서와 타입이 일치하는 생성자 필요)
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                            .getResultList();
 
-            /*
-            <<query.getSingleResult()>>
-                - 결과가 정확히 하나, 단일 객체 반환
-                - 결과가 없으면 NoResultException
-                - 둘 이상이면 NonUniqueResultException
-            TypedQuery<Member> query = em.createQuery("select m from Member m where m.id = 10", Member.class);
-            Member result = query.getSingleResult();
-
-            System.out.println("member1 = " + result);
-            */
-
-            /*
-            <<파라미터 바인딩>>
-             - 이름 기반
-            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter("username", "member1")
-                    .getSingleResult();
-            System.out.println("result = " + result.getUsername());
-            */
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO = " + memberDTO.getUsername());
+            System.out.println("memberDTO = " + memberDTO.getAge());
 
             tx.commit();
         } catch (Exception e) {
