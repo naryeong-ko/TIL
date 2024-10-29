480a3c583e4d15cb1666331110307e9516766213
naryeong-ko <naryung26@gmail.com>
2024-10-29 10:44:50 +0900

    [Algo] 기본 문제 풀이
    [Algo] 프로그래머스 컨트롤 제트 문제 풀이

---
+++ 알고리즘/기본/컨트롤 제트.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(String s) {
+        String[] ss = s.split(" ");
+        int answer = 0;
+
+        for(int i = 0; i < ss.length; i++) {
+            if(ss[i].equals("Z")) {
+                answer -= Integer.parseInt(ss[i - 1]);
+            } else {
+                answer += Integer.parseInt(ss[i]);
+            }
+        }
+
+        return answer;
+    }
+}
