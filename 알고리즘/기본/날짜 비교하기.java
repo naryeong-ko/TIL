76730267595597643789215a01a7d0ebe88fdd17
naryeong-ko <naryung26@gmail.com>
2024-12-01 14:45:08 +0900

    [Algo] 기본 문제 풀이
    [Algo] 프로그래머스 날짜 비교하기 풀이

---
+++ 알고리즘/기본/날짜 비교하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        for(int i = 0; i < 3; i++) {
+            if(date1[i] > date2[i]) {
+                return 0;
+            }
+
+            if(date1[i] < date2[i]) {
+                return 1;
+            }
+        }
+
+        return 0;
+    }
+}
