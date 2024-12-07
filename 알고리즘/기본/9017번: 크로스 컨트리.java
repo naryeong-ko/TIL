ae4e63f3fe0745fdfbdfe2508616841be131372c
naryeong-ko <naryung26@gmail.com>
2024-12-07 14:25:22 +0900

    [Algo] 기본 문제 풀이 수정
    [Algo] BOJ 9017번: 크로스 컨트리 문제 풀이 수정

--- 알고리즘/기본/9017번: 크로스 컨트리.java
+++ 알고리즘/기본/9017번: 크로스 컨트리.java
@@ -9,27 +9,37 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
 
         for(int tc = 0; tc < T; tc++) {
-            int[][] ans = new int[201][3];
-            
-            int[] max = {-1, 201, 201};
+            int[] cnt = new int[201];
+            int[][] sum = new int[201][3];// 개수, 합, 5번 원소
+            List<Integer> list = new ArrayList<>();
             int N = Integer.parseInt(br.readLine());
+            int[] nums = new int[N + 1];
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             for(int i = 1; i <= N; i++) {
-                int n = Integer.parseInt(st.nextToken());
-                ans[n][0]++;
-                if(ans[n][0] <= 4) {
-                    ans[n][1] += i;
-                } else if(ans[n][0] == 5) {
-                    ans[n][2] = i;
+                nums[i] = Integer.parseInt(st.nextToken());
+                cnt[nums[i]]++;
+            }
+
+            int idx = 1;
+            for(int i = 1; i <= N; i++) {
+                if(cnt[nums[i]] == 6) {
+                    if(++sum[nums[i]][0] < 5) {
+                        sum[nums[i]][1] += idx;
+                    } else if(sum[nums[i]][0] == 5) {
+                        sum[nums[i]][2] = idx;
+                    }
+
+                    idx++;
                 }
             }
 
+            int[] max = {0, 1000, 201}; // 1등인 팀 번호, 합, 5번 원소
             for(int i = 1; i <= 200; i++) {
-                if(ans[i][0] == 6 && (max[1] > ans[i][1] || (max[1] == ans[i][1] && max[2] > ans[i][2]))) {
+                if(sum[i][0] == 6 && (sum[i][1] < max[1] || (sum[i][1] == max[1] && sum[i][2] < max[2]))) {
                     max[0] = i;
-                    max[1] = ans[i][1];
-                    max[2] = ans[i][2];
+                    max[1] = sum[i][1];
+                    max[2] = sum[i][2];
                 }
             }
 
