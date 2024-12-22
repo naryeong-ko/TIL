3506524971182f73ed7c38343aaa92a6f0179dd6
naryeong-ko <naryung26@gmail.com>
2024-12-22 23:56:22 +0900

    [Algo] 기본 문제 풀이
    [Algo] 프로그래머스 주사위 게임 2 문제 풀이

---
+++ 알고리즘/기본/주사위 게임 2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = a + b + c;
+
+        if(a == b && a == c) {
+            return answer * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c);
+        } else if(a == b || a == c || b == c) {
+            return answer * (a * a + b * b + c * c);
+        }
+
+        return answer;
+    }
+}
