fcc0fed4dcc8cb21772484314de0a33b259998df
naryeong-ko <naryung26@gmail.com>
2025-03-10 00:10:52 +0900

    [Algo] 기본 문제 풀이
    [Algo] 프로그래머스 글자 이어 붙여 문자열 만들기 문제 풀이

---
+++ 알고리즘/기본/글자 이어 붙여 문자열 만들기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int[] index_list) {
+        StringBuilder answer = new StringBuilder();
+
+        for(int i : index_list) {
+            answer.append(my_string.charAt(i));
+        }
+
+        return answer.toString();
+    }
+}
