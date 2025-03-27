988c2d42af6ca56ac4669f54619df763a0195c09
naryeong-ko <naryung26@gmail.com>
2025-03-27 21:11:46 +0900

    [Algo] 기본 문제 풀이
    [Algo] 프로그래머스 조건에 맞게 수열 변환하기 3 문제 풀이

---
+++ 알고리즘/기본/조건에 맞게 수열 변환하기 3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        int[] answer = new int[arr.length];
+
+        if(k % 2 == 0) {
+            for(int i = 0; i < arr.length; i++) {
+                answer[i] = arr[i] + k;
+            }
+
+            return answer;
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+            answer[i] = arr[i] * k;
+        }
+
+        return answer;
+    }
+}
